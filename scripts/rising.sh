eff2e9edee4be2f44d09f5bf06fd15a47c41bb95
Demi <kizura.vian@gmail.com>
2024-11-01 18:35:30 +0800

    Update rising.sh

--- scripts/rising.sh
+++ scripts/rising.sh
@@ -5,7 +5,6 @@ rm -rf hardware/qcom-caf/sm8150/media && \
 rm -rf vendor/lineage && \
 rm -rf hardware/xiaomi && \
 git clone https://github.com/musivian/local_manifest.git -b main .repo/local_manifests && \
-export BUILD_RISING=true
 . build/envsetup.sh && \
 riseup sunny user && \
 rise b && \
