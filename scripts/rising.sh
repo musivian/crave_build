659bc4e46fdf9ff1bfccd3af1fe28e89fd06990a
Demi <kizura.vian@gmail.com>
2024-11-01 12:09:14 +0800

    Update rising.sh

--- scripts/rising.sh
+++ scripts/rising.sh
@@ -1,20 +1,17 @@
 rm -rf .repo/local_manifests ; \
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b fourteen -g default,-mips,-darwin,-notdefault && \
-git clone https://github.com/musivian/local_manifest.git -b main .repo/local_manifests && \
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs && \
 /opt/crave/resync.sh && \
 rm -rf hardware/qcom-caf/sm8150/media && \
 rm -rf vendor/lineage && \
 rm -rf hardware/xiaomi && \
+git clone https://github.com/musivian/local_manifest.git -b main .repo/local_manifests && \
 export BUILD_RISING=true
-source build/envsetup.sh && \
+. build/envsetup.sh && \
 riseup sunny user && \
-rise b > /dev/null && \
-mv out/target/product/sunny/RisingOS*.zip . && \
+rise b && \
 export WITH_GMS=true && \
 export TARGET_DEFAULT_PIXEL_LAUNCHER=true && \
-rise b > /dev/null && \
-mv out/target/product/sunny/RisingOS*.zip . && \
+rise b && \
 export TARGET_CORE_GMS=true && \
 export TARGET_CORE_GMS_EXTRAS=true && \
-rise b > /dev/null && \
-mv out/target/product/sunny/RisingOS*.zip .
+rise b
